ed0042ed2329f284f748c5848fda47cc804d97b8
kyria-lydia <kyriabereaukorah@gmail.com>
2023-03-01 08:20:59 -0800

    Commit message

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <studio.h>
+
+/**
+ * main - prints alpha in lower and upper case
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+
+	puchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+project low level programming variableso
